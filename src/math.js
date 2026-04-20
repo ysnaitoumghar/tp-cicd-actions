63f479dab938bbd3891e41b7e485c917fff6c793
ysnaitoumghar <y.aitoumghar7483@uca.ac.ma>
2026-04-20 23:18:02 +0100

    feat: init project with math module and tests

---
+++ src/math.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function factorial(n) {
+    if (n < 0) throw new Error('Nombre négatif');
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
+
+module.exports = { add, multiply, factorial };
